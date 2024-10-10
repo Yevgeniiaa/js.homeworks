9e8afed2395379e9403b0b6efaa8e731c68d9f1a
Yevgeniiaa <138613772+Yevgeniiaa@users.noreply.github.com>
2024-10-10 19:10:12 +0300

    Add files via upload

---
+++ script3.js
@@ -0,0 +1,31 @@
+console.clear();
+
+let inputs = document.querySelectorAll("input")
+let answers = document.querySelectorAll(".answer")
+
+function task1(){
+    let number = inputs[0].value;
+    answers[0].textContent = number
+}
+
+
+let email = "yevgenia20032007@gmail.com"
+
+function task2(){
+    let number = inputs[1].value;
+    let email = str.length(email)
+    answers[1].textContent = number
+}
+
+function task3(){
+    let number = inputs[2].value;
+    let email = str.length(email)
+    answers[2].textContent = number
+}
+
+let userName = ""
+function task4(){
+    let number = inputs[3].value;
+    let email = str.length(email)
+    answers[3].textContent = number
+}
